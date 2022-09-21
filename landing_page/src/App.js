5a3541580777bd53360d031a4e6205393178ac54
Amanda Laureano <ens-amandacruz@uniguacu.edu.br>
2022-09-21 13:29:24 -0300

    desenvolvimento pt.2

--- landing_page/src/App.js
+++ landing_page/src/App.js
@@ -3,46 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="main">
-        <div className="center">
-          <div className="menu">
-            <div>
-              <h3>Astronomia Blog</h3>
-            </div>
-            <div className="item-menu">
-              <a href="#">Login</a>
-            </div>
-          </div>
-
-          <div className="form">
-            <h2>Se inscreva em nossa newsletter!</h2>
-            <form>
-              <div className="itens-form">
-                <input type="text" placeholder="Digite seu nome" />
-                <input type="text" placeholder="E-mail" />
-                <input type="button" value="Enviar"/>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>  
-
-      <div className="conteudo">
-        <div className="center">
-          <div className="conteudo-single">
-            <h3>Título da chamada</h3>
-            <p>
-              Aqui vai o conteúdo
-            </p>
-          </div>
-          <div className="conteudo-single">
-            <h3>Título da chamada</h3>
-            <p>
-              Aqui vai o conteúdo
-            </p> 
-          </div>  
-        </div>
-      </div>
+      
     </div>
   );
 }
---
+++ landing_page/src/Main.js
@@ -0,0 +1,167 @@
+function Main(){
+    return(
+        <div className="main">
+        <div className="center">
+          <div className="menu">
+            <div>
+              <h3>Astronômo Blog</h3>
+              <div className="item-menu">
+                <input type="button" className="buttonLogin" value="Login"/>
+              </div>
+            </div>
+          </div>
+
+          <div className="form">
+            <h2>Se inscreva em nossa newsletter!</h2>
+            <form>
+              <div className="itens-form">
+                <input type="text" className="campotxt" placeholder="Digite seu nome" />
+                <input type="text" className="campotxt"placeholder="E-mail" />
+                <input type="button" className="button" value="Enviar"/>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>  
+      <div className="conteudo">
+        <div className="center">
+          <div className="titulo_pagina">
+            <h2> 12 Objetos astronômicos que vão acabar com seu complexo de superioridade</h2>
+            <p>Se você dá muita importância aos problemas que sua mente inventa ou se acha o centro de tudo, acredite, há coisas maiores por aí. Estes doze objetos astronônomicos o farão refletir sobre o seu real tamanho e o de seus problemas no pequeno mundo em que vivemos.</p>
+          </div>
+          
+          <div class="card w-50">
+            <div>
+              <img src="" className="amostra"  alt="" />
+            </div>
+            <div class="card-body">
+              <h3 class="card-title">Universo escuro</h3>
+              <p class="card-text">Mesmo sendo o centro de tudo o que podemos ver, a verdade é que não vemos muito. Segundo a teoria mais aceita hoje, o Universo visível, incluindo todos os planetas, estrelas, galáxias, aglomerados, superaglomerados e os ainda hipotéticos hiperaglomerados, correspondem a menos de 5% do que existe de fato. O resto é composto por algo ainda muito misterioso - a matéria escura e sua correlata energia escura. O que os cientistas sabem sobre elas é que não emitem ou refletem luz e, portanto, nos são invisíveis. A evidência que se tem de sua existência está nos efeitos gravitacionais que elas excercem sobre os objetos visíveis. A partir desses efeitos, conclui-se que há uma "matéria faltante"", algo que não podemos ver, mas que está lá.</p>
+              <a href="#" class="btn btn-primary">Button</a>
+            </div>
+          </div> 
+
+          <div class="card w-50">
+            <div>
+              <img className="amostra" src="" alt="" />
+            </div>
+            <div class="card-body">
+              <h3 class="card-title">Buracos Negros</h3>
+              <p class="card-text">Acredita-se que todas as galáxias tenham em seu centro gravitacional um buraco negro colossal, capaz de carregar, com sua gravidade, centenas de bilhões de estrelas e um número inimaginável de objetos menores orbitando ao seu redor. A nossa Via láctea não é diferente. Cientistas estimam que o buraco negro super-massivo do centro de nossa galáxia tenha massa equivalente a 3 milhões de vezes a do Sol.</p>
+              <a href="#" class="btn btn-primary">Button</a>
+            </div>
+          </div>   
+
+          <div class="card w-50">
+            <div>
+              <img className="amostra" src="" alt="" />
+            </div>
+            <div class="card-body">
+              <h3 class="card-title">Quasar</h3>
+              <p class="card-text">Os quasares são objetos imensos, com buracos negros em seus centros, que aparentam estar expelindo jatos de matéria gigantescos. Eles são os objetos mais luminosos e energéticos conhecidos, emitindo entre 100 e 1000 vezes a luz de uma galáxia inteira, muitas vezes observados no centro de galáxias jovens e ativas. O quasar SDSS J0100 + 2802, encontrado recentemente, tem, em seu centro, um buraco negro com massa estimada em 12 bilhões de vezes a do Sol.</p>
+              <a href="#" class="btn btn-primary">Button</a>
+            </div>
+          </div>   
+
+          <div class="card w-50">
+            <div>
+              <img className="amostra" src="" alt="" />
+            </div>
+            <div class="card-body">
+              <h3 class="card-title">Galáxia fominha</h3>
+              <p class="card-text">A IC 1101 é uma galáxia elíptica gigante no centro do aglomerado de Abell 2029. Ela tem diâmetro aproximado de 6 milhões de anos-luz. É a maior galáxia conhecida atualmente. Estima-se que IC 1101 aglomere cerca de 300 trilhões de estrelas, mais de mil vezes o número da Via Láctea, com população aproximada de 200 bilhões. Se fosse colocada no lugar de nossa galáxia, engoliria diversos objetos próximos, como a Galáxia de Andrômeda e a Galáxia do Triângulo.</p>
+              <a href="#" class="btn btn-primary">Button</a>
+            </div>
+          </div>  
+
+          <div class="card w-50">
+            <div>
+              <img className="amostra" src="" alt="" />
+            </div>
+            <div class="card-body">
+              <h3 class="card-title">Planeta Godzilla</h3>
+              <p class="card-text">O exoplaneta Kepler-10c, apelidado de Godzilla, é o maior planeta rochoso já encontrado no Universo. Com 17 vezes a massa da Terra e 2,3 vezes o seu diâmetro, o exoplaneta surpreendeu astrônomos, que não imaginavam que um planeta rochoso poderia ser tão massivo. O que se pensava é que tal objeto aglutinaria o material ao seu redor e tonaria-se um gigante gasoso, como Júpiter ou Saturno.</p>
+              <a href="#" class="btn btn-primary">Button</a>
+            </div>
+          </div>
+
+          <div class="card w-50">
+            <div>
+              <img className="amostra" src="" alt="" />
+            </div>
+            <div class="card-body">
+              <h3 class="card-title">Planeta Cortiça</h3>
+              <p class="card-text">TrES-4 é o maior exoplaneta gasoso conhecido. Estima-se que seu diâmetro seja 170% maior que o de Júpiter, o maior planeta do Sistema Solar. O objeto é também um dos mais curiosos já encontrados. Apesar de gigantesco, ele é anormalmente leve, tendo uma densidade de apenas 0,2 gramas por centímetro cúbico - semelhante à de uma rolha. Por essa razão, ganhou o apelido de Planeta-cortiça.</p>
+              <a href="#" class="btn btn-primary">Button</a>
+            </div>
+          </div> 
+
+          <div class="card w-50">
+            <div>
+              <img className="amostra" src="" alt="" />
+            </div>
+            <div class="card-body">
+              <h3 class="card-title">Órbita de Sedna</h3>
+              <p class="card-text">Você costuma reclamar que o ano está passando muito rápido? Bom, você está certo. Além de todos os planetas do Sistema Solar, muito depois de Plutão, encontra-se Sedna, um objeto que astrônomos imaginam ser um planeta-anão. Não é exatamente o tamanho de Sedna o que impressiona – acredita-se que ele tenha aproximadamente 1.000 quilômetros de diâmetro -, mas a grandeza de sua órbita. O planetoide demora algo em torno de 11.400 anos para dar uma volta completa em nosso Sol.</p>
+              <a href="#" class="btn btn-primary">Button</a>
+            </div>
+          </div> 
+
+          <div class="card w-50">
+            <div>
+              <img className="amostra" src="" alt="" />
+            </div>
+            <div class="card-body">
+              <h3 class="card-title">Gigante vermelha</h3>
+              <p class="card-text">A gigantesca UY Scuti é a maior estrela conhecida, com um volume próximo a 5 bilhões de vezes o do Sol. O astro é tão grande que, se estivesse no centro do Sistema Solar, ocuparia a órbita de todos os planetas interiores e ultrapassaria a de Júpiter, quase alcançando Saturno. A estrela é uma super-gigante vermelha e está a cerca de 9.500 anos-luz da Terra, na constelação do Escudo</p>
+              <a href="#" class="btn btn-primary">Button</a>
+            </div>
+          </div> 
+
+          <div class="card w-50">
+            <div>
+              <img className="amostra" src="" alt="" />
+            </div>
+            <div class="card-body">
+              <h3 class="card-title">Grupo local</h3>
+              <p class="card-text">O Grupo Local começa uma categoria diferente e pouco conhecida – a dos aglomerados de galáxias. Na verdade, mesmo contendo 54 galáxias, o Grupo é apenas um pedaço do aglomerado ao qual pertence. Ele engloba, além da Via Láctea e da Galáxia de Andrômeda, seus principais centros de gravidade, outras galáxias próximas, como a Galáxia do Triângulo e as Nuvens de Magalhães</p>
+              <a href="#" class="btn btn-primary">Button</a>
+            </div>
+          </div>    
+
+          <div class="card w-50">
+            <div>
+              <img className="amostra" src="" alt="" />
+            </div>
+            <div class="card-body">
+              <h3 class="card-title">Aglomerado de Virgem</h3>
+              <p class="card-text">Virgem é um grande aglomerado de galáxias que contém entre 1.300 e 2.000 desses objetos. Sua massa estimada é de 1,2 mil trilhões a do Sol. Ele influencia, com sua gravidade, o Grupo Local e milhares de outras galáxias em seus determinados conjuntos gravitacionais. Até os anos 1980, acreditava-se que os aglomerados seriam as maiores estruturas astronômicas aglutinadas pela gravidade</p>
+              <a href="#" class="btn btn-primary">Button</a>
+            </div>
+          </div>     
+
+          <div class="card w-50">
+            <div>
+              <img className="amostra" src="" alt="" />
+            </div>
+            <div class="card-body">
+              <h3 class="card-title">Superaglomerado de Virgem</h3>
+              <p class="card-text">Também chamado de Superaglomerado Local, ele é dominado pelo Aglomerado de Virgem, em seu centro, e possui mais cerca de 100 aglomerados de galáxias. Acredita-se que tenha a forma de um disco plano, com aproximadamente 200 milhões de anos-luz de diâmetro. Apesar de seu tamanho inconcebível, ele é apenas um dos inúmeros superaglomerados existentes no Universo e está longe de ser o maior deles</p>
+              <a href="#" class="btn btn-primary">Button</a>
+            </div>
+          </div>     
+
+          <div class="card w-50">
+            <div>
+              <img className="amostra" src="" alt="" />
+            </div>
+            <div class="card-body">
+              <h3 class="card-title">Universo visível</h3>
+              <p class="card-text">Se depois de conhecer a imensidão das superestruturas do Universo você estiver se sentindo menor que um filhote de ácaro anão, anime-se! Você é, de fato, o centro do Universo. Pelo menos do Universo observável. A luz tem uma velocidade limitada e, portanto, o centro do Universo visível tem de ser o seu observador. Considerando que o Universo tem 13,7 bilhões de anos-luz de idade, todos os itens anteriores, ou pelo menos a luz de tudo o que observamos no céu, com os telescópios mais potentes, foi emitida de sua origem há 13,7 bilhões de anos. Isso significa também, que boa parte dos objetos que podemos ver não existe mais ou mudou de posição há muito tempo. Acredita-se que há algo em torno de 30 a 70 bilhões de trilhões de estrelas no Universo visível e 80 bilhões de galáxias. Considerando a constante expansão do Universo, pode-se afirmar, também, que a parte visível teria hoje um diâmetro de 92 bilhões de anos-luz</p>
+              <a href="#" class="btn btn-primary">Button</a>
+            </div>
+          </div>               
+        </div>
+      </div>
+    )
+}
